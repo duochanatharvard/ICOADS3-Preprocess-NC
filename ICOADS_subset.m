ff795f9c6027c1f6dc9ad6611bb2d31f05648592
duochanatharvard <duochan@.harvard.edu>
2021-07-09 07:13:05 -0400

    Update ICOADS_subset to omit variable if subset fails.

--- ICOADS_subset.m
+++ ICOADS_subset.m
@@ -5,6 +5,10 @@ function [P,var_list] = ICOADS_subset(P,l_use)
     var_list = fieldnames(P);
     
     for var = 1:numel(var_list)
-        eval(['P.',var_list{var},' = P.',var_list{var},'(l_use,:);']);
+        try
+            eval(['P.',var_list{var},' = P.',var_list{var},'(l_use,:);']);
+        catch
+            disp([var_list{var},' is not subset'])
+        end
     end
 end
