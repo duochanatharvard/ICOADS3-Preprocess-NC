fba301a0405f427394e0268b778a53b6d8af4172
duochanatharvard <duochan@.harvard.edu>
2022-11-17 15:30:59 -0500

    3.0.1 -> 3.0.2

--- ICOADS_NC_version.m
+++ ICOADS_NC_version.m
@@ -2,6 +2,6 @@ function ICOADS_version = ICOADS_NC_version(yr)
     if yr <= 2014
         ICOADS_version = '3.0.0';
     else
-        ICOADS_version = '3.0.1';
+        ICOADS_version = '3.0.2';
     end
 end
